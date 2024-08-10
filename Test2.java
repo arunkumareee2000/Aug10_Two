f3cfd03b64ea858525bb704c449cca837848a7b2
arunkumareee2000 <arunkumareee2000@gmail.com>
2024-08-10 23:14:46 +0530

    new line added

--- Test2.java
+++ Test2.java
@@ -1,6 +1,7 @@
 public class Test2{
     public static void main(String args[]){
         System.out.println("This is test2.java file create today");
+        System.out.println("Good bye I'm going to sleep");
 
     }
 }

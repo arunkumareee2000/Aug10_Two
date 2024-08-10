4c28db82ed704b1114dde08836308e308902519c
arunkumareee2000 <59086860+arunkumareee2000@users.noreply.github.com>
2024-08-10 22:49:32 +0530

    Update Test2.java

--- Test2.java
+++ Test2.java
@@ -1,5 +1,5 @@
 public class Test2{
     public static void main(String args[]){
-        System.out.println("This is test2.java file create today");
+        System.out.println("This is test2.java file create today on Aug10");
     }
 }
